d86a523e05480cfa8055d51983c8b0eca9652ea4
aaronkeith <aflisik@gmail.com>
2017-10-12 11:37:24 -0700

    move index.html, index.js, styles.css into public folder

--- public/index.js
+++ public/index.js
@@ -7,6 +7,15 @@ const app = express()
 MongoClient.connect('mongodb://localhost/library', function(err, db) {
   app.use(express.static('public'))
   app.use(bodyParser.json())
+
+  app.get('http://ergast.com/api/f1/2017/1/qualifying', (req, res) => {
+    const f1data = db.collection('f1data')
+    f1data.find({}, (err, result) => {
+      result.toArray().then(data => {
+        res.json(data)
+      })
+    })
+  })
 })
 
 app.listen(3000)
