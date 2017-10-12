18c1d844fc663bad4df6fcfb9f6c35de4b23a2af
aaronkeith <aflisik@gmail.com>
2017-10-12 11:08:31 -0700

    required MongoClient, express, and body-parser within index.js Connected MongoClient #1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars */
+const { MongoClient } = require('mongodb')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+MongoClient.connect('mongodb://localhost/library', function(err, db) {
+  app.use(express.static('public'))
+  app.use(bodyParser.json())
+})
